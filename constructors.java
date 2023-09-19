e98f82fbd5ad755fc731580092cb0d4a02dbae38
StewieC <129508680+StewieC@users.noreply.github.com>
2023-09-19 12:14:31 +0300

    Create constructors.java
    Simple code explaining how to easily create constructors in java

---
+++ constructors.java
@@ -0,0 +1,19 @@
+package thiscontsuctors;
+
+public class Thiss {
+	public static void main(String[]args) {
+	// create an object	
+		Student koshy = new Student();
+	}
+}
+
+//create a student class
+class Student{
+	int reg;
+	int marks;
+	// create constructors below to initiate your data
+	Student(){
+		this.reg = 0;
+		this.marks = 0;
+	}
+}
